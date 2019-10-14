eb49bbb8777f61c90b3dc349e0ba2feba3b11688
Michael Stizza <mikestizza@gmail.com>
2019-10-14 11:22:44 -0400

    all chang

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPV4 IP address:")
+
+## the line below creates a single stirng that is passed to print()
+# print("You told me the IPV4 address is:" + user_input)
+
+## print() can be given a series of objects seperated by a comma 
+print ("You told me the IPV4 address is:", user_input)
---
+++ no_shebang.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+print("hello there")
+print("did you say hello?")
+print("No, I said hello there. But that's close enough!")
+print("Here's a third line just because.")
+
